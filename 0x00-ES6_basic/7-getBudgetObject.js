362cbe3a562b83cf8ee045283b32e57bda5e3e97
Victorinox2 <uwaomaviktor@yahoo.com>
2024-03-07 01:43:25 +0000

    modify object value

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: $119,868,
+    gdp: $154.2 billion,
+    capita: $178,479,
+  };
+
+  return budget;
+}
