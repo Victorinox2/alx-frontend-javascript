7da012c2822bbe69a4eb409fd2d8b8f7c7374d7b
Victorinox2 <uwaomaviktor@yahoo.com>
2024-03-08 01:59:29 +0000

    create iterator object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,8 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
 
-  return {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        const employeesInCurrentDepartment = allEmployees[currentDepartment];
-
-        if (currentEmployeeIndex < employeesInCurrentDepartment.length) {
-          const currentEmployee = employeesInCurrentDepartment[currentEmployeeIndex];
-          currentEmployeeIndex++;
-          return { value: currentEmployee, done: false };
-        } else {
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next();  // Recursive call for the next department
-        }
-      } else {
-        return { done: true };
-      }
-    }
-  };
+  return myarray;
 }
