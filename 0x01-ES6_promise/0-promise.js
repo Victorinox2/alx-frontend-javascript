094c2cc9113edaf3c90bc1ca1978e7eb5804ac46
Victorinox2 <uwaomaviktor@yahoo.com>
2024-03-11 14:03:05 +0000

    add ES6 Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,20 @@
-function function getResponseFromAPI() {
-        return new Promise((resolve, reject) => {
-
-                if ("succesful")
-                {
-                        resolve("success");
-                }
-                else
-                {
-                        reject("failuere");
-                }
-        });
+function getResponseFromAPI(){
+	return new Promise((resolve, reject) => {
+	const success = true;
+		if ("success")
+		{
+			resolve("operation successful");
+		}
+		else
+		{
+			reject("operation failed");
+		}
+	});
 }
+
+getResponseFromAPI.then((result) => {
+	console.log(result);
+})
+.catch ((error) => {
+	console.error(error);
+})
