a8da3a0111a7489e530cff689cfcbab3a1d0cd04
Victorinox2 <uwaomaviktor@yahoo.com>
2024-03-11 14:23:08 +0000

    add ES6 Prototype

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,22 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true; 
-      if (success) {
-        // If successful, resolve the Promise with the result
-        resolve("API response data");
-      } else {
-        // If there's an error, reject the Promise with an error message
-        reject("Error: Unable to get response from the API");
-      }
-    }, 2000);
-  });
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
 }
-
-getResponseFromAPI()
-  .then(response => {
-    console.log("Success:", response);
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
